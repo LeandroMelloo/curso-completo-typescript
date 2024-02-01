34cf5b9b3b010ad101e7b6f89cd50ac782bb3e67
LeandroMelloo <leandromoreno1909@hotmail.com>
2024-02-01 11:35:41 -0300

    Aula de comentarios em JS

---
+++ comentarios/index.js
@@ -0,0 +1,9 @@
+// Escrever um comentário
+console.log("Escrever um comentário");
+// Aqui tem outro comentário
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+console.log("Linha 5");
+// Está linha será ignorada
---
+++ console/exercicio.js
@@ -0,0 +1,5 @@
+console.log(
+  'Meu nome é "Leandro". Estou aprendendo JavaScript às',
+  10,
+  "da manhã."
+);
